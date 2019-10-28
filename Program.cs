c0e4f863cb21af8a8a928284a1af6dded75ba713
Yana <joanna27@mail.ru>
2019-10-28 10:52:52 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversesWords
+{
+    static class StringHelper
+    {
+        /// <summary>
+        /// Receives string and returns the string with its letters reversed.
+        /// </summary>
+        public static string ReverseString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StringHelper.ReverseString("The greatest victory is that which requires no battle"));
+            Console.ReadLine();
+        }
+    }
+}
